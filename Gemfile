8635f7e5aa1adac037c70e5668f471bacbeac9ff
mayiPalacios <mayipalacios2@gmail.com>
2023-09-27 21:33:00 -0600

    add routes methods

--- Gemfile
+++ Gemfile
@@ -43,7 +43,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'dotenv-rails'
-  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+    gem 'faker', '~> 2.18'
   gem 'factory_bot_rails'
   gem 'rspec'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
+  
+  resources :users, only: [:show] do
+    resources :tweets, only: [:index]
+  end
 
+  resources :users do
+    resources :tweets, only: [:new, :create, :update]
+  end
+
+  resources :tweets do
+    resources :likes, only: [:new, :create, :destroy]
+  end
+
+  resources :tweets do
+    member do
+      post "quote"
+      post "retweet"
+    end
+  end
+
+  resources :tweets do
+    resources :replies, only: [:create], param: :tweet_id
+  end
+
+  resources :tweets do
+    resources :bookmarks, only: [:create, :destroy], on: :member
+  end
+
+  resources :users do
+    get "tweets", on: :member, as: :tweets, defaults: { page: 1 }
+  end
+
+  resources :users do
+    get "tweets_and_replies", on: :member, as: :tweets_and_replies, defaults: { page: 1 }
+  end
+
+  resources :tweets do
+    get "stats", on: :member, as: :tweet_stats
+  end
 end
