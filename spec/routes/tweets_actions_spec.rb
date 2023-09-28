bfbe328e7134732704a8c64cecd777beb3972bf5
mayiPalacios <mayipalacios2@gmail.com>
2023-09-28 13:44:30 -0600

    fix tweet_action method

--- spec/routes/tweets_actions_spec.rb
+++ spec/routes/tweets_actions_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 RSpec.describe "TweetsActions", type: :routing do
   describe "POST /tweets/:id/quote" do
     it("creates a new quoted tweet succesfully ") do
-    expect(post("/tweets/2/quote")).to route_to("tweets#quote",id: "2")
-  end
+      expect(post("/tweets/2/quote")).to route_to("tweets#quote",id: "2")
+    end
   end
 
   describe "POST /tweets/:id/retweet" do
